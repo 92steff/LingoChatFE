e763ec4ed3add6268a0b305fda08191eced78815
Stefan <marjanovic.steff@gmail.com>
2019-09-19 19:49:57 +0200

    adding user profile

--- src/app/user/user-profile/user-profile.component.ts
+++ src/app/user/user-profile/user-profile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { User } from 'src/app/models/user.model';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { FormGroup, FormControl } from '@angular/forms';
+import * as fromApp from '../../store/app.reducers';
+import * as UserSelectors from '../../auth/store/auth.selectors';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +12,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.less']
 })
 export class UserProfileComponent implements OnInit {
+  user: User;
+  userDataForm: FormGroup;
+  passChangeForm: FormGroup;
+  isEditing: boolean = false;
+  isPassEditing: boolean = false;
 
-  constructor() { }
+  constructor(private store: Store<fromApp.AppState>, private jwtS: JwtHelperService) { }
 
   ngOnInit() {
+    this.store.select(UserSelectors.selectToken).subscribe((token) => {
+      this.user = this.jwtS.decodeToken(token);
+    })
+    this.userDataForm = new FormGroup({
+      firstName: new FormControl(this.user.firstName),
+      lastName: new FormControl(this.user.lastName),
+      email: new FormControl(this.user.email)
+    })
+    this.passChangeForm = new FormGroup({
+      oldPassword: new FormControl(null),
+      newPassword: new FormControl(null),
+      reNewPassword: new FormControl(null)
+    }, { validators: this.passMatch, updateOn: 'submit' })
+  }
+
+  changeInfo() {
+    console.log('It woooorks!');
+  }
+
+  changePassword() {
+    
+  }
+
+  cancelEdit() {
+    this.userDataForm.reset({
+      firstName: this.user.firstName,
+      lastName: this.user.lastName,
+      email: this.user.email
+    })
+    this.isEditing = !this.isEditing;
+  }
+
+  cancelPassEdit() {
+    this.passChangeForm.reset();
+    this.isPassEditing = !this.isPassEditing;
+  }
+
+  passMatch(control: FormGroup) {
+    if (control.get('newPassword').value === control.get('reNewPassword').value) {
+      return null;
+    }
+    return { 'passDontMatch': true };
   }
 
 }
